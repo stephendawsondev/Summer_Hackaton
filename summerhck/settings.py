d6805631f1eca5b7c9e06acc1bfbed38c0273f11
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2023-07-12 20:46:30 +0000

    Mod: secret Key

--- summerhck/settings.py
+++ summerhck/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+if os.path.isfile('env.py'):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=cg1$il!ch8amb3sx5e$m69j5+mci^%9-jv9xm(uucbaal0=3w"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
