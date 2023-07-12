7d0403efb77dc17157228319ff86225eafefcddf
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2023-07-12 21:16:34 +0000

    Edit: Allowed Hosts

--- summerhck/settings.py
+++ summerhck/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://sungood-d5e69927dc92.herokuapp.com/",
+    '127.0.0.1',
+    'localhost'
+    ]
+
 
 
 # Application definition
