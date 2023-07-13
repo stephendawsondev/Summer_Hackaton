99e7dc4b6531d4889a7293868ccd2af2737cddd1
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2023-07-13 22:17:04 +0000

    Set: Database

--- summerhck/settings.py
+++ summerhck/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 import os
 if os.path.isfile('env.py'):
@@ -95,11 +96,8 @@ WSGI_APPLICATION = "summerhck.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+DATABASES = {    
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')), 
 }
 
 # Cloudinary storage
